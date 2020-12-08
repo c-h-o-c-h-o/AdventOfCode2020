dd830dd934eef20380bb10c65b0f0b851d4dc367
Justin Cho <justch0616@gmail.com>
2020-12-08 13:52:19 -0600

    Fixed extra indent

--- AoCDay8.py
+++ AoCDay8.py
@@ -6,18 +6,18 @@ with open('Day8.txt', 'r') as data:
 accumulator = 0
 i = 0
 while data[i] != 'fin':
-        if data[i][0:3] == 'acc':
-            accumulator += int(data[i][4:])
-            data[i] = 'fin'
-            i += 1 
-        if data[i][0:3] == 'nop':
-            accumulator += 0
-            data[i] = 'fin'
-            i += 1
-        if data[i][0:3] == 'jmp':
-            add = int(data[i][4:])
-            data[i] = 'fin'
-            i += add
+    if data[i][0:3] == 'acc':
+        accumulator += int(data[i][4:])
+        data[i] = 'fin'
+        i += 1 
+    if data[i][0:3] == 'nop':
+        accumulator += 0
+        data[i] = 'fin'
+        i += 1
+    if data[i][0:3] == 'jmp':
+        add = int(data[i][4:])
+        data[i] = 'fin'
+        i += add
 
 print(accumulator)
 
