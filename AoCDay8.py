9c141f4a2a5c821d0f9e65bcd629005ab9667d95
Justin Cho <justch0616@gmail.com>
2020-12-08 13:50:44 -0600

    Day 8 Part 1

---
+++ AoCDay8.py
@@ -0,0 +1,23 @@
+with open('Day8.txt', 'r') as data:
+    data = data.read()
+    data = data.split('\n')
+#print(data)
+
+accumulator = 0
+i = 0
+while data[i] != 'fin':
+        if data[i][0:3] == 'acc':
+            accumulator += int(data[i][4:])
+            data[i] = 'fin'
+            i += 1 
+        if data[i][0:3] == 'nop':
+            accumulator += 0
+            data[i] = 'fin'
+            i += 1
+        if data[i][0:3] == 'jmp':
+            add = int(data[i][4:])
+            data[i] = 'fin'
+            i += add
+
+print(accumulator)
+
