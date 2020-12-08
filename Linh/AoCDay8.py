efdacbea2dfd39a48f7a6c75e5e2ceb2572146b0
Linh Nguyen <nguy4006@umn.edu>
2020-12-08 13:50:10 -0600

    hailie folder

---
+++ Linh/AoCDay8.py
@@ -0,0 +1,24 @@
+with open('Day8.txt', 'r') as data:
+    data = data.read()
+    data = data.split('\n')
+
+#Part 1
+
+accum = 0
+
+i = 0 
+while data[i] != 'fin':
+    if data[i][:3] == 'acc':
+        accum += int(data[i][4:])
+        data[i] = 'fin'
+        i += 1
+    if data[i][:3] == 'nop':
+        data[i] = 'fin'
+        i += 1        
+    if data[i][:3] == 'jmp':
+        add = int(data[i][4:])
+        data[i] = 'fin'
+        i += add
+    print(accum)
+
+print(accum)
